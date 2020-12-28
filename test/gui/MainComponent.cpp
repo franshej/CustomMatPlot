d325678a875b7998f29ea6fbf9d69c7433b2fcac
Frans Rosencrantz <frans.rosencrantz@dirac.com>
2020-12-28 23:25:37 +0100

    Moving test macros from src to header

--- test/gui/MainComponent.cpp
+++ test/gui/MainComponent.cpp
@@ -1,79 +1,7 @@
 #include "MainComponent.h"
 #define PI2 6.28318530718
 
-#define PLOT_Y(Y)                                                              \
-  {                                                                            \
-    ((MainComponent *)thiz)                                                    \
-        ->get_plot_holder()                                                    \
-        ->push_back({std::make_unique<LinearPlot>(), test_name});              \
-    ((MainComponent *)thiz)->get_plot_holder()->back().first->updateYData(Y);  \
-    thiz->addAndMakeVisible(                                                   \
-        ((MainComponent *)thiz)->get_plot_holder()->back().first.get());       \
-  }
-
-#define PLOT_XY(X, Y)                                                          \
-  {                                                                            \
-    ((MainComponent *)thiz)                                                    \
-        ->get_plot_holder()                                                    \
-        ->push_back({std::make_unique<LinearPlot>(), test_name});              \
-    ((MainComponent *)thiz)->get_plot_holder()->back().first->updateYData(Y);  \
-    ((MainComponent *)thiz)->get_plot_holder()->back().first->updateXData(X);  \
-    thiz->addAndMakeVisible(                                                   \
-        ((MainComponent *)thiz)->get_plot_holder()->back().first.get());       \
-  }
-
-#define X_LIM(MIN, MAX)                                                        \
-  ((MainComponent *)thiz)->get_plot_holder()->back().first->xLim(MIN, MAX);
-
-#define Y_LIM(MIN, MAX)                                                        \
-  ((MainComponent *)thiz)->get_plot_holder()->back().first->yLim(MIN, MAX);
-
-struct node {
-  void (*fun_ptr)(juce::Component *comp,
-                  const std::string &test_name) = nullptr;
-  std::string name;
-  node *next = NULL;
-  node *tail = NULL;
-} *head = NULL;
-
-static void g_test_add(void (*new_fun_ptr)(juce::Component *comp,
-                                           const std::string &test_name),
-                       const std::string &test_name) {
-  if (head == NULL) {
-    head = new node;
-    head->fun_ptr = new_fun_ptr;
-    head->tail = head;
-    head->name = test_name;
-  } else {
-    head->tail->next = new node;
-    head->tail->next->fun_ptr = new_fun_ptr;
-    head->tail->next->name = test_name;
-    head->tail = head->tail->next;
-  }
-}
-
-#ifdef __cplusplus
-#define TEST(f)                                                                \
-  static void f(juce::Component *thiz, const std::string &test_name);          \
-  struct f##_t_ {                                                              \
-    f##_t_(void) { g_test_add(&f, #f); }                                       \
-  };                                                                           \
-  static f##_t_ f##_;                                                          \
-  static void f(juce::Component *thiz, const std::string &test_name)
-#else
-#define TEST(f)                                                                  \
-  static void f(juce::Component *thiz);                                          \
-  static void __attribute__((constructor(101)) __construct_##f(void) {        \
-    g_test_add(&f);                                                            \
-  }                                                                            \
-  static void f(juce::Component *thiz)
-#endif
-
-#define ADD_PARENT_COMP(COMP)                                                  \
-  struct node *cur;                                                            \
-  for (cur = head; cur; cur = cur->next) {                                     \
-    cur->fun_ptr(COMP, cur->name);                                             \
-  }
+extern node *head = NULL;
 
 TEST(test_flat_curve_1000) {
   std::vector<float> y_test_data(10000);
@@ -159,64 +87,24 @@ MainComponent::MainComponent() : m_menu_label("", "Tests: ") {
   for (auto &plot : m_plot_holder) {
     plot.first->setBounds(0, getScreenArea().getHeight() / 15, getWidth(),
                           getHeight() - getScreenArea().getHeight() / 15);
-	plot.first->setVisible(false);
+    plot.first->setVisible(false);
   }
 
   m_test_menu.onChange = [this]() {
-	  if (m_current_plot != nullptr) {
-		  m_current_plot->setVisible(false);
-	  }
-	  const auto id = m_test_menu.getSelectedId();
-	  if (!m_plot_holder.empty()) {
-		  m_current_plot = m_plot_holder[id - 1].first.get();
-		  m_current_plot->setVisible(true);
-	  }
+    if (m_current_plot != nullptr) {
+      m_current_plot->setVisible(false);
+    }
+    const auto id = m_test_menu.getSelectedId();
+    if (!m_plot_holder.empty()) {
+      m_current_plot = m_plot_holder[id - 1].first.get();
+      m_current_plot->setVisible(true);
+    }
   };
 }
 
-/*
-
-
-void test_y_lim(juce::Graphics& g)
-{
-    std::vector<PlotData> test_data;
-    test_data.push_back(PlotData());
-    test_data.back().y_values = std::vector<float>(100);
-    std::iota(test_data.back().y_values.begin(),
-test_data.back().y_values.end(), 0); for (auto& y : test_data.back().y_values)
-    {
-        y = std::sin(y * PI2 / test_data.back().y_values.size());
-    }
-    LinearPlot plot(400, 200, 200, 100);
-    plot.yLim(-1, 0);
-    plot.plot(g, test_data);
-}
-*/
 void MainComponent::paint(juce::Graphics &g) {
   g.fillAll(
       getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
-  /*
-
-std::iota(test_data.back().y_values.begin(), test_data.back().y_values.end(),
-0); test_data.back().x_values = std::vector<float>(10);
-std::iota(test_data.back().x_values.begin() + 5,
-test_data.back().x_values.end(), 0); test_data.back().x_values[0] = -3;
-test_plot3.plot(test_data);
-
-test_data.back().x_values = std::vector<float>(10);
-for (auto i = 0u; i < test_data.back().x_values.size(); ++i)
-{
-test_data.back().x_values[i] = i * 20;
-}
-test_data.back().x_values[0] = 3;
-test_plot4.plot(test_data);
-
-test_simple_ramp(g);
-test_sinus_auto_lim(g);
-test_two_sinus_auto_lim(g);
-test_x_lim(g);
-test_y_lim(g);
-*/
 }
 
 void MainComponent::resized() {
--- test/gui/MainComponent.h
+++ test/gui/MainComponent.h
@@ -3,19 +3,96 @@
 #include <juce_gui_extra/juce_gui_extra.h>
 #include <spl_plot.h>
 
+struct node {
+  void (*fun_ptr)(juce::Component *comp,
+                  const std::string &test_name) = nullptr;
+  std::string name;
+  node *next = NULL;
+  node *tail = NULL;
+};
+
+extern node *head;
+
+#define ADD_PARENT_COMP(COMP)                                                  \
+  struct node *cur;                                                            \
+  for (cur = head; cur; cur = cur->next) {                                     \
+    cur->fun_ptr(COMP, cur->name);                                             \
+  }
+
+static void g_test_add(void (*new_fun_ptr)(juce::Component *comp,
+                                           const std::string &test_name),
+                       const std::string &test_name) {
+  if (head == NULL) {
+    head = new node;
+    head->fun_ptr = new_fun_ptr;
+    head->tail = head;
+    head->name = test_name;
+  } else {
+    head->tail->next = new node;
+    head->tail->next->fun_ptr = new_fun_ptr;
+    head->tail->next->name = test_name;
+    head->tail = head->tail->next;
+  }
+}
+
+#ifdef __cplusplus
+#define TEST(f)                                                                \
+  static void f(juce::Component *thiz, const std::string &test_name);          \
+  struct f##_t_ {                                                              \
+    f##_t_(void) { g_test_add(&f, #f); }                                       \
+  };                                                                           \
+  static f##_t_ f##_;                                                          \
+  static void f(juce::Component *thiz, const std::string &test_name)
+#else
+#define TEST(f)                                                                  \
+  static void f(juce::Component *thiz);                                          \
+  static void __attribute__((constructor(101)) __construct_##f(void) {        \
+    g_test_add(&f);                                                            \
+  }                                                                            \
+  static void f(juce::Component *thiz)
+#endif
+
+#define PLOT_Y(Y)                                                              \
+  {                                                                            \
+    ((MainComponent *)thiz)                                                    \
+        ->get_plot_holder()                                                    \
+        ->push_back({std::make_unique<LinearPlot>(), test_name});              \
+    ((MainComponent *)thiz)->get_plot_holder()->back().first->updateYData(Y);  \
+    thiz->addAndMakeVisible(                                                   \
+        ((MainComponent *)thiz)->get_plot_holder()->back().first.get());       \
+  }
+
+#define PLOT_XY(X, Y)                                                          \
+  {                                                                            \
+    ((MainComponent *)thiz)                                                    \
+        ->get_plot_holder()                                                    \
+        ->push_back({std::make_unique<LinearPlot>(), test_name});              \
+    ((MainComponent *)thiz)->get_plot_holder()->back().first->updateYData(Y);  \
+    ((MainComponent *)thiz)->get_plot_holder()->back().first->updateXData(X);  \
+    thiz->addAndMakeVisible(                                                   \
+        ((MainComponent *)thiz)->get_plot_holder()->back().first.get());       \
+  }
+
+#define X_LIM(MIN, MAX)                                                        \
+  ((MainComponent *)thiz)->get_plot_holder()->back().first->xLim(MIN, MAX);
+
+#define Y_LIM(MIN, MAX)                                                        \
+  ((MainComponent *)thiz)->get_plot_holder()->back().first->yLim(MIN, MAX);
+
 class MainComponent : public juce::Component {
 public:
   MainComponent();
 
   void paint(juce::Graphics &) override;
   void resized() override;
-  std::vector<std::pair<std::unique_ptr<Plot>, std::string>> *get_plot_holder() {
+  std::vector<std::pair<std::unique_ptr<Plot>, std::string>> *
+  get_plot_holder() {
     return &m_plot_holder;
   }
 
 private:
   std::vector<std::pair<std::unique_ptr<Plot>, std::string>> m_plot_holder;
-  Plot* m_current_plot = nullptr;
+  Plot *m_current_plot = nullptr;
   juce::ComboBox m_test_menu;
   juce::Label m_menu_label;
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
