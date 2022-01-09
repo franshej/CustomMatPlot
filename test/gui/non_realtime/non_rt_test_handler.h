768f98de1bda06f27a6108e597386f5c7e9aa9a9
Frans Rosencrantz <frans.rosencrantz@gmail.com>
2022-01-09 11:23:34 +0100

    Refactor test module

--- test/gui/non_realtime/non_rt_test_handler.h
+++ test/gui/non_realtime/non_rt_test_handler.h
@@ -1,32 +1,7 @@
 #pragma once
 
-#include <juce_gui_extra/juce_gui_extra.h>
 #include <spl_plot.h>
-
-static scp::Plot *getPlotFromID(
-    std::map<std::string, std::unique_ptr<scp::Plot>> &plot_holder,
-    const int id) {
-  if (id >= plot_holder.size()) return nullptr;
-
-  auto it = plot_holder.begin();
-
-  for (int i = 1; i < plot_holder.size() + 1; ++i) {
-    if (id == i) break;
-    it++;
-  }
-
-  return it->second.get();
-}
-
-static juce::Rectangle<int> getScreenArea() {
-  return juce::Desktop::getInstance().getDisplays().getMainDisplay().userArea;
-}
-
-struct node {
-  std::function<void(juce::Component *, const std::string &)> fun_ptr = nullptr;
-  std::string name;
-  std::shared_ptr<node> next = nullptr;
-};
+#include "test_utils.h"
 
 class NonRTTestHandler : public juce::Component {
  public:
