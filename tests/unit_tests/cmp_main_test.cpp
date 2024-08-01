063a82dea2d2acd53e5a02e0afc58a1c2f9ff619
Frans Rosencrantz <frans.rosencrantz@gmail.com>
2024-08-01 13:53:12 +0200

    Tests: Add unit test for cmp_plot

---
+++ tests/unit_tests/cmp_main_test.cpp
@@ -0,0 +1,10 @@
+#include <juce_core/juce_core.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+
+int main()
+{
+    auto gui_scope = juce::ScopedJuceInitialiser_GUI();
+    juce::UnitTestRunner runner;
+    runner.runAllTests();
+    return 0;
+}
