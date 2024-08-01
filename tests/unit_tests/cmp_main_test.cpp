bcecf3a6b19981cdf9feced7afd8e3d11043bc8e
Frans Rosencrantz <frans.rosencrantz@gmail.com>
2024-08-01 14:45:45 +0200

    Refactor: Add test macros for unit tests

--- tests/unit_tests/cmp_main_test.cpp
+++ tests/unit_tests/cmp_main_test.cpp
@@ -1,10 +1,9 @@
 #include <juce_core/juce_core.h>
 #include <juce_gui_basics/juce_gui_basics.h>
 
-int main()
-{
-    auto gui_scope = juce::ScopedJuceInitialiser_GUI();
-    juce::UnitTestRunner runner;
-    runner.runAllTests();
-    return 0;
+int main() {
+  auto gui_scope = juce::ScopedJuceInitialiser_GUI();
+  juce::UnitTestRunner runner;
+  runner.runAllTests();
+  return 0;
 }
