dff42431ebaa2e29fcaf241adb548209bc120589
Frans Rosencrantz <frans.rosencrantz@gmail.com>
2024-08-30 11:06:59 +0200

    CI: Build and run tests

--- tests/unit_tests/cmp_main_test.cpp
+++ tests/unit_tests/cmp_main_test.cpp
@@ -5,5 +5,14 @@ int main() {
   auto gui_scope = juce::ScopedJuceInitialiser_GUI();
   juce::UnitTestRunner runner;
   runner.runAllTests();
+  const auto num_tests = runner.getNumResults();
+
+  for (int i = 0; i < num_tests; ++i) {
+    auto result = runner.getResult(i);
+    if (result->failures > 0) {
+      return 1;
+    }
+  }
+
   return 0;
 }
